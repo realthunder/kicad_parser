ed20f2f15efeb632e935a0e6f60a6efc6393eb5d
Zheng, Lei <realthunder.dev@gmail.com>
2017-01-06 01:02:00 +0800

    Added missing layers parsing

--- kicad_pcb.py
+++ kicad_pcb.py
@@ -32,6 +32,11 @@ class KicadPCB_pad(SexpParser):
     __slots__ = ()
     _parse1_drill = KicadPCB_drill
 
+    def _parse1_layers(self,data):
+        if not isinstance(data,list) or len(data)<3:
+            raise ValueError('expects list of more than 2 element')
+        return Sexp(data[1],data[2:],data[0])
+
 
 class KicadPCB_module(SexpParser):
     __slots__ = ()
@@ -43,24 +48,25 @@ class KicadPCB_module(SexpParser):
 class KicadPCB(SexpParser):
 
     # To make sure the following key exists, and is of type SexpList
-    _defaults = \
-            'net',\
-            ('net_class',
-                'add_net'),\
-            'dimension',\
-            'gr_text',\
-            'gr_line',\
-            'gr_circle',\
-            'gr_arc',\
-            'segment',\
-            'via',\
-            ('module',\
-                'fp_text',\
-                'fp_line',\
-                'fp_circle',\
-                'fp_arc',\
-                'pad',\
-                'model')
+    _defaults =('net',
+                ('net_class',
+                    'add_net'),
+                'dimension',
+                'gr_text',
+                'gr_line',
+                'gr_circle',
+                'gr_arc',
+                'segment',
+                'via',
+                ('module',
+                    'fp_text',
+                    'fp_line',
+                    'fp_circle',
+                    'fp_arc',
+                    'pad',
+                    'model'),
+                ('zone',
+                    'filled_polygon'))
 
     _parse_module = KicadPCB_module
     _parse_gr_text = KicadPCB_gr_text
