112ee9302bc19ec562cd80e80fd6e47409b000e0
Leandro Heck <leoheck@gmail.com>
2022-06-13 11:55:57 -0300

    Revert imports

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 
 from kicad_pcb import *
-
-try:
-    from .sexp_parser import *
-except ImportError:
-    from sexp_parser.sexp_parser import *
+from sexp_parser import *
 
 import sys
 import argparse
